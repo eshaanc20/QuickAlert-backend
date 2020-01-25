a2af930ff07fc7d5cb6067a0d1b917c25b302749
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-25 16:25:00 -0500

    Updated backend

--- routes/sms.js
+++ routes/sms.js
@@ -10,19 +10,18 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 router.post('/', function(req, res, next) {
     // const twiml = new MessagingResponse();
     
-    User.find({phoneNumber: req.body.From}).then(userInfo => {
-        const userInfo = {
-            firstName: userInfo.firstName,
-            lastName: userInfo.lastName,
-            phoneNumber: userInfo.phoneNumber,
-            age: userInfo.age,
-            conditions: userInfo.conditions,
-            otherDetails: userInfo.otherDetails,
-        }
-        const Alert = NewAlert('NewAlerts', userInfo, 'Saturday', req.body.Body);
-        Alert.save();
-    })
-
+    // User.find({phoneNumber: req.body.From}).then(userInfo => {
+    //     const userInfo = {
+    //         firstName: userInfo.firstName,
+    //         lastName: userInfo.lastName,
+    //         phoneNumber: userInfo.phoneNumber,
+    //         age: userInfo.age,
+    //         conditions: userInfo.conditions,
+    //         otherDetails: userInfo.otherDetails,
+    //     }
+    //     const Alert = NewAlert('NewAlerts', userInfo, 'Saturday', req.body.Body);
+    //     Alert.save();
+    // })
     // twiml.message('Your information has been sent to ' + hospitalName)
     // res.writeHead(200, {'Content-Type': 'text/xml'});
     // res.end(twiml.toString());
