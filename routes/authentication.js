665ec3d6ed323f4cc04672b2f513557729bfeb22
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-25 16:58:49 -0500

    fixed authentication

--- routes/authentication.js
+++ routes/authentication.js
@@ -7,7 +7,7 @@ router.post('/', (req, res, next) => {
     if (req.body.type == 'hospital') {
         Hospital.find({ email: req.body.email }).then(info => {
             res.send(req.body.password)
-            if (info.password == req.body.password) {
+            if (info[0].password == req.body.password) {
                 res.send(true);
             } else {
                 res.send(false);
@@ -18,12 +18,11 @@ router.post('/', (req, res, next) => {
     }
     else if (req.body.type == 'user') {
         User.find({ email: req.body.email }).then(info => {
-            res.send(info)
-            // if (info.password == req.body.password) {
-            //     res.send(true);
-            // } else {
-            //     res.send(false);
-            // }
+            if (info[0].password == req.body.password) {
+                res.send(true);
+            } else {
+                res.send(false);
+            }
         }).catch(err => {
             res.send("User not in database")
         })
