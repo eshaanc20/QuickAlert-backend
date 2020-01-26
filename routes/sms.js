140a01c67a41fabb9c56ea2e20195df1d46e11ff
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 05:35:10 -0500

    Added geocoding for sms

--- routes/sms.js
+++ routes/sms.js
@@ -7,28 +7,54 @@ var {User} = require('../db/mongoose');
 var {Service} = require('../db/mongoose');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+const distanceBetween = (coordinates1, coordinates2) => {
+    [lat1, long1] = coordinates1;
+    [lat2, long2] = coordinates2;
+    var lat1 = lat1.toRadians();
+    var lat2 = lat2.toRadians();
+    var differenceLat = (lat2-lat1).toRadians();
+    var differenceLong = (long2-long1).toRadians;
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) *Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    return 6371 * c;
+}
+
 router.post('/', function(req, res, next) {
     const twiml = new MessagingResponse();
     
     User.find({phoneNumber: req.body.From}).then(userInfo => {
         var currentTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+        const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + req.body.Body + ".json?access_token=pk.eyJ1IjoiZXNoYWFuYyIsImEiOiJjazV1Z2RieDYxOWo1M21tanVpdmlxbG54In0.0WdUZzxQ-wDgly1Q44y4lA"
         currentTime = new Date(currentTime);
-        const userInformation = {
-            name: userInfo[0].name,
-            time: currentTime,
-            currentLocation: req.body.Body,
-            serviceName: 'goodwills',
-            phoneNumber: userInfo[0].phoneNumber,
-            age: userInfo[0].age,
-            medicalConditions: userInfo[0].medicalConditions,
-            otherDetails: userInfo[0].otherDetails,
-        }
-        let newAlert = new Alert(userInformation)
-        newAlert.save().then(() => {
-            // twiml.message('Your information has been sent to ' + 'Goodwill University')
-            // res.writeHead(200, {'Content-Type': 'text/xml'});
-            // res.end(twiml.toString());
-        })
+        axios.get(url)
+            .then(response => {
+                [long, lat] = response.data.features[0].center
+                Service.find({}).then(services => {
+                    const distances = []
+                    services.forEach(service => {
+                        const distance = distanceBetween([service.latitude, service.longitude], [lat, long]);
+                        distances.push(distance);
+                    })
+                    const index = distances.indexOf(Math.min(distances));
+                    serviceSelected = services[index]
+                    const userInformation = {
+                        name: userInfo[0].name,
+                        time: currentTime,
+                        currentLocation: req.body.Body,
+                        serviceName: serviceSelected.name,
+                        phoneNumber: userInfo[0].phoneNumber,
+                        age: userInfo[0].age,
+                        medicalConditions: userInfo[0].medicalConditions,
+                        otherDetails: userInfo[0].otherDetails,
+                    }
+                    let newAlert = new Alert(userInformation)
+                    newAlert.save().then(() => {
+                        twiml.message('Your information has been sent to ' + serviceSelected.name)
+                        res.writeHead(200, {'Content-Type': 'text/xml'});
+                        res.end(twiml.toString());
+                    })
+                })
+            })
     })
 })
 
