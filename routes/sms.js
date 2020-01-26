a83432ca77366fb51143c1ead35e4f0388cfa41d
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 01:30:10 -0500

    Updated route

--- routes/sms.js
+++ routes/sms.js
@@ -11,20 +11,20 @@ router.post('/', function(req, res, next) {
     const twiml = new MessagingResponse();
     
     User.find({phoneNumber: req.body.From}).then(userInfo => {
-        const userInformation = {
-            firstName: userInfo.firstName,
-            lastName: userInfo.lastName,
-            phoneNumber: userInfo.phoneNumber,
-            age: userInfo.age,
-            medicalConditions: userInfo.medicalConditions,
-            otherDetails: userInfo.otherDetails,
-        }
-        const Alert = NewAlert("goodwilluniversity");
-        const newAlert = new Alert(userInformation);
-        // twiml.message('Your information has been sent to ' + 'Goodwill University')
-        // res.writeHead(200, {'Content-Type': 'text/xml'});
-        // res.end(twiml.toString());
-        Alert.save();
+        // const userInformation = {
+        //     firstName: userInfo.firstName,
+        //     lastName: userInfo.lastName,
+        //     phoneNumber: userInfo.phoneNumber,
+        //     age: userInfo.age,
+        //     medicalConditions: userInfo.medicalConditions,
+        //     otherDetails: userInfo.otherDetails,
+        // }
+        // const Alert = NewAlert("goodwilluniversity");
+        // const newAlert = new Alert(userInformation);
+        twiml.message('Your information has been sent to ' + 'Goodwill University')
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+        // Alert.save();
     })
 })
 
