3e86178c6364abda79e64a072ee4c27e0f8c7a65
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 01:04:49 -0500

    Adjusted routes

--- routes/sms.js
+++ routes/sms.js
@@ -21,9 +21,9 @@ router.post('/', function(req, res, next) {
         }
         const Alert = NewAlert("goodwilluniversity");
         const newAlert = new Alert(userInformation);
-        twiml.message('Your information has been sent to ' + 'Goodwill University')
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
+        // twiml.message('Your information has been sent to ' + 'Goodwill University')
+        // res.writeHead(200, {'Content-Type': 'text/xml'});
+        // res.end(twiml.toString());
         Alert.save();
     })
 })
