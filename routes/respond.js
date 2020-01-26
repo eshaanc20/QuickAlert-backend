ee1216b6d9a401f0ec3c9617c63d8e76bb740c76
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 02:20:23 -0500

    update

--- routes/respond.js
+++ routes/respond.js
@@ -3,13 +3,14 @@ var router = express.Router();
 const {NewAlert} = require('../db/mongoose.js');
 
 router.get('/', function(req, res, next) {
-    const Alert = NewAlert(req.body.name)
-    Alert.find({}).then(alerts => {
-        var JSONdata = JSON.stringify(alerts);
-        res.send(JSONdata);
-    }).catch(err => {
-        res.send("No alerts");
-    })
+    res.send("hi")
+    // const Alert = NewAlert(req.body.name)
+    // Alert.find({}).then(alerts => {
+    //     var JSONdata = JSON.stringify(alerts);
+    //     res.send(JSONdata);
+    // }).catch(err => {
+    //     res.send("No alerts");
+    // })
 });
 
 module.exports = router;
