bc1528492634e8147b0b9ee4385889d3d6c545ac
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 06:20:35 -0500

    update

--- routes/sms.js
+++ routes/sms.js
@@ -13,12 +13,12 @@ router.post('/', function(req, res, next) {
     const distanceBetween = (coordinates1, coordinates2) => {
         [lat1, long1] = coordinates1;
         [lat2, long2] = coordinates2;
-        // var lat1 = lat1.toRadians();
-        // var lat2 = lat2.toRadians();
-        // var differenceLat = (lat2-lat1).toRadians();
-        // var differenceLong = (long2-long1).toRadians();
-        // var a = Math.sin(differenceLat/2) * Math.sin(differenceLat/2) + Math.cos(lat1) * Math.cos(lat2) *Math.sin(differenceLong/2) * Math.sin(differenceLong/2);
-        // var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+        var lat1 = lat1 * Math.PI / 180;
+        var lat2 = lat1 * Math.PI / 180;
+        var differenceLat = (lat2-lat1) * Math.PI / 180;
+        var differenceLong = (long2-long1) * Math.PI / 180;
+        var a = Math.sin(differenceLat/2) * Math.sin(differenceLat/2) + Math.cos(lat1) * Math.cos(lat2) *Math.sin(differenceLong/2) * Math.sin(differenceLong/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
         return lat1;
     }    
     
