f3117e0d6705319c0b13186e9bf4056ee5fa5e0a
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 02:21:31 -0500

    Fixed error with respond router

--- routes/respond.js
+++ routes/respond.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 const {NewAlert} = require('../db/mongoose.js');
 
-router.get('/', function(req, res, next) {
-    res.send("hi")
-    // const Alert = NewAlert(req.body.name)
-    // Alert.find({}).then(alerts => {
-    //     var JSONdata = JSON.stringify(alerts);
-    //     res.send(JSONdata);
-    // }).catch(err => {
-    //     res.send("No alerts");
-    // })
+router.post('/', function(req, res, next) {
+    const Alert = NewAlert(req.body.name)
+    Alert.find({}).then(alerts => {
+        var JSONdata = JSON.stringify(alerts);
+        res.send(JSONdata);
+    }).catch(err => {
+        res.send("No alerts");
+    })
 });
 
 module.exports = router;
