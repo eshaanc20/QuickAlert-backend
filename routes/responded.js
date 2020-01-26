42151fb4347e2f8a124469976a1f183a33746e0c
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-01-26 09:30:05 -0500

    Fixed error

--- routes/responded.js
+++ routes/responded.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const {Alert} = require('../db/mongoose.js');
 
 router.post('/', function(req, res, next) {
-    Alert.updateOne({name: req.body.name}, {responded: true}).then(() => {
+    Alert.updateMany({name: req.body.name}, {responded: true}).then(() => {
         res.send("Updated")
     }).catch(err => {
         res.send("Error")
