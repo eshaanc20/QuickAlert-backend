ee19a3b11027c5ce7e01ede9a64ba7a305fb97a5
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-03-01 18:16:48 -0500

    Added route for updating

---
+++ routes/update.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.router();
+var {User} = require('../db/mongoose')
+
+router.get('/', (req, res, next) => {
+    User.updateOne({name: req.body.email}, {
+        ...req.body
+    }).then(() => {
+        res.send('Database updated')
+    })
+});
+
+module.exports = router;
