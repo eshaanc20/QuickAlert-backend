d231c6521e8aa6030e9a1f3890ee26ed00f12f77
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-03-01 19:04:41 -0500

    Debug

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var newUserRouter = require('./routes/newUser');
 var newServiceRouter = require('./routes/newService');
 var respondRouter = require('./routes/respond');
 var respondedRouter = require('./routes/responded');
-var updateRouter = require('./routes/update');
 
 var app = express();
 
@@ -35,7 +34,6 @@ app.use('/newUser', newUserRouter);
 app.use('/newService', newServiceRouter);
 app.use('/respond', respondRouter);
 app.use('/responded', respondedRouter);
-app.use('/update', updateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
