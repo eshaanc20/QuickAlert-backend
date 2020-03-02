033c9584cceccb9542c8bf47846a9997fe33e08f
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-03-01 19:06:45 -0500

    Cors error

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
