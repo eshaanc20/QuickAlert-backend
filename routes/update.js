9dc8c227b3d4b80a4b671f4d94544d1525426a85
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2020-03-01 19:49:53 -0500

    Fixed error with update route

--- routes/update.js
+++ routes/update.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var {User} = require('../db/mongoose')
 
-router.post('/', (req, res, next) {
+router.post('/', (req, res, next) => {
     User.updateOne({name: req.body.email}, {
         ...req.body
     }).then(() => {
